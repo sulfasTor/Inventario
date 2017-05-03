cd64c1e22cb00ccf553ce1d70690307d38a2f336
sulfasTor <moisesabra@hotmail.com>
2017-05-03 23:16:48 +0200

    print 2 pdf works

--- jspdf.js
+++ jspdf.js
@@ -1,23 +1,7 @@
-function html2pdf(){
+function generatePDF(){
 
-  var doc = new jsPDF({
-    orientation: 'landscape'
+  var pdf = new jsPDF('landscape');
+  pdf.addHTML($('#table')[0], function () {
+  pdf.save('inventario.pdf');
   });
-
-  source = $('body').get(0);
-
-  // We'll make our own renderer to skip this editor
-  var specialElementHandlers = {
-  	'#editor': function(element, renderer){
-  		return true;
-  	}
-  };
-
-  // All units are in the set measurement for the document
-  // This can be changed to "pt" (points), "mm" (Default), "cm", "in"
-  doc.fromHTML(source, 15, 15, {
-  	'width': 180,
-  	'elementHandlers': specialElementHandlers
-  });
-    doc.save("inventario.pdf");
 }
