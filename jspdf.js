ca558343e0864a476e98f1019f96ce6169e370f0
sulfasTor <moisesabra@hotmail.com>
2017-05-04 14:30:52 +0200

    remove by id works

--- jspdf.js
+++ jspdf.js
@@ -1,7 +1,7 @@
 function generatePDF(){
 
-  var pdf = new jsPDF('landscape');
-  pdf.addHTML($('#table')[0], function () {
-  pdf.save('inventario.pdf');
-  });
+    var pdf = new jsPDF('landscape');
+    pdf.addHTML($('#table')[0], function () {
+	pdf.save('inventario.pdf');
+    });
 }
